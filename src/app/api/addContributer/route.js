a04a9b5312141846e3293d94f62dc809b7685e9a
kiran <kiranugale239@gmail.com>
2024-10-19 09:07:47 +0530

    card update

--- src/app/api/addContributer/route.js
+++ src/app/api/addContributer/route.js
@@ -5,20 +5,28 @@ export async function POST(req) {
   try {
     const { data, id } = await req.json();
 
+    console.log("Received data:", data);
+    console.log("Event ID:", id);
+
     const createContribute = await Contributers.create({
       name: data.name,
       class_Name: data.class_Name,
       date: data.date,
       amount: data.amount,
     });
+
+    console.log("Created Contributor:", createContribute);
+
     const updateEventContibuter = await Events.findOneAndUpdate(
-      { _id: id?.id }, // Filter to find the document
+      { _id: id?.id },
       {
         $push: {
           contributors: createContribute?._id,
         },
       }
     );
+
+    console.log("Updated Event Contributor:", updateEventContibuter);
 
     if (updateEventContibuter) {
       await Events.updateOne(
@@ -36,13 +44,15 @@ export async function POST(req) {
     } else {
       return NextResponse.json({
         success: false,
+        message: "Event not found or not updated",
       });
     }
   } catch (error) {
-    console.log(error);
+    console.log("Error occurred:", error);
 
     return NextResponse.json({
       success: false,
+      message: "An error occurred while processing your request.",
     });
   }
 }
